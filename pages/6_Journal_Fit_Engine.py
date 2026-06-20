a200ba5e2a2dc5c265e8bc905d34af05fc0daafe
Liang (Arthur) Li <arthur312@hotmail.com>
2026-06-20 14:07:16 -0400

    Add pages/6_Journal_Fit_Engine.py

---
+++ pages/6_Journal_Fit_Engine.py
@@ -0,0 +1,8 @@
+import sys, os
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TOOL = os.path.join(ROOT, "06_journal_fit_engine")
+sys.path.insert(0, ROOT)
+sys.path.insert(0, TOOL)
+os.chdir(TOOL)
+with open(os.path.join(TOOL, "app.py")) as _f:
+    exec(_f.read(), {"__file__": os.path.join(TOOL, "app.py")})
