708a640bef16d287c65634071dcd4f7cc2432f94
Liang (Arthur) Li <arthur312@hotmail.com>
2026-06-20 14:07:16 -0400

    Add pages/7_Synthesis_Matrix_Generator.py

---
+++ pages/7_Synthesis_Matrix_Generator.py
@@ -0,0 +1,8 @@
+import sys, os
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TOOL = os.path.join(ROOT, "07_synthesis_matrix_generator")
+sys.path.insert(0, ROOT)
+sys.path.insert(0, TOOL)
+os.chdir(TOOL)
+with open(os.path.join(TOOL, "app.py")) as _f:
+    exec(_f.read(), {"__file__": os.path.join(TOOL, "app.py")})
